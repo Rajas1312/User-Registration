80629ad8483d859a6910efdbb174e2ccf09972eb
Rajas1312 <itsmerajs2@gmail.com>
2020-11-05 16:17:16 +0530

    valid last name

--- userReg.sh
+++ userReg.sh
@@ -11,3 +11,13 @@ then
 else
 	echo "invalid"
 fi
+
+echo " Enter last name "
+read input2
+last_name_pattern="^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$"
+if [ $input2=~$last_name_pattern ]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
