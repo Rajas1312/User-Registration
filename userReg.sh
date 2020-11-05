1ff61737d9d82700872d54ce1dfa3d4a4323c240
Rajas1312 <itsmerajs2@gmail.com>
2020-11-05 16:00:10 +0530

    valid irst name

--- userReg.sh
+++ userReg.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to user Registration"
 
+
+echo " Enter first name "
+read input1
+first_name_pattern="^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*$"
+if [ $input1=~$first_name_pattern ]
+then
+	echo "valid"
+else
+	echo "invalid"
+fi
